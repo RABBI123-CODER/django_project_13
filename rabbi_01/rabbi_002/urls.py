cd198b84c627b50d17d7613f19fdf9e2558cdda8
MD RABIUAL HASAN <rabiualhasanrabbi123@gmail.com>
2023-08-25 01:20:22 +0600

    workin in database in rabbi_001 admin.py and create superuser

---
+++ rabbi_01/rabbi_002/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('se/', views.rabbi_0002, name = 'rabbi_2')
+]
---
+++ rabbi_01/rabbi_002/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def rabbi_0002(request):
+    return render(request, 'rabbi_002/rabbi_002.html')
